8ef551a5db2181e9988bbc12d38e647f35e1c176
max <ovdvibe@gmail.com>
2019-12-31 18:48:32 +0900

    lol

--- client/jasc.js
+++ client/jasc.js
@@ -16,11 +16,30 @@ console.log(window)
 //innerHeight/pageYOffset<2
 //aka half or less
 
-    if((window.innerHeight/window.pageYOffset)<2)
+var winHeight=window.innerHeight;
+var scroll=window.pageYOffset;
 
-    var target = document.querySelector(".landcnt")
-    var rate=window.pageYOffset * -0.1
+//navbar opacity
+    if((winHeight/scroll)<2&&winHeight/scroll>1){
+        document.querySelector(".navbar").style.opacity=`${(winHeight/scroll*-5/18)+(0.75+2*(5/18))}`;
 
+        //1.25-(0.25*winHeight/scroll)
+
+       // 2   .75
+        //1.1 1
+        //0= .75+2(5/18)   aka about 1.3
+
+  //      winHeight/scroll*-5/18+(0.75+2(5/18)) 
+
+    //    rate*-5/18+(0.75+2(5/18)) 
+    }else if(winHeight/scroll<1){
+        document.querySelector(".navbar").style.opacity="1"
+    }else{
+        document.querySelector(".navbar").style.opacity="0.75";
+    }
+
+
+    //navbar visibility
     if(window.pageYOffset>50&&hidden){
         document.getElementById("navbar").classList.remove("nav-hidden");
         hidden=!hidden;
@@ -30,8 +49,13 @@ console.log(window)
         hidden=!hidden;
     }
 
+    //title parallax
+    var target = document.querySelector(".landcnt")
+    var rate=window.pageYOffset * -0.1
     target.style.transform=`translate3d(0px,${rate}px, 0px)`
     
+
+    //title opacity
     if(rate>-25){
         target.style.opacity="1"
     }
